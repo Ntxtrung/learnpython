d897aab245a4167d356eebaa9a0cdeb6df25939c
ntxtrung <ntxtrung@gmail.com>
2023-08-01 15:58:24 +0700

    add set file

---
+++ set.py
@@ -0,0 +1,8 @@
+my_set = set(['Vinamation', True, 'hoc', 5, 'com', 'Vinamation'])
+print(my_set)
+
+for x in my_set:
+    print(f" {x}", end=' ')
+
+print("hello" in my_set)
+print("Vinamation" in my_set)
---
+++ set_add.py
@@ -0,0 +1,10 @@
+my_set = {'Vinamation', True, 5, 'com'}
+
+my_set1 = {2, 3, 4, 5}
+
+my_set.update(my_set1)
+print(f"Add a set to my_set: {my_set}")
+
+my_set.discard("com")
+print(f"remove a set to my_set: {my_set}")
+
